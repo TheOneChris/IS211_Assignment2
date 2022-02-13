d35cbccfc6bddefc195ec017572de92a4a9d453c
TheOneChris <60333802+TheOneChris@users.noreply.github.com>
2022-02-12 20:22:56 -0500

    Fully works code Assignment 2
    works

--- assignment2.py
+++ assignment2.py
@@ -3,6 +3,10 @@ import urllib.request
 import logging
 import datetime
 
+logging.basicConfig(filename="errors.log", level=logging.ERROR)
+assignment2 = logging.getLogger()
+
+
 def downloadData(url):
     """Downloads the data"""
     with urllib.request.urlopen(url) as response:
@@ -14,27 +18,41 @@ def downloadData(url):
 def processData(file_content):
     person_dict = {}
     for data_line in file_content.split("\n"):
+        if data_line.strip() == "":
+            continue
         id, name, birthday = data_line.split(",")
         if id == "id":
             continue
-        id_int = int(id)
         try:
             real_birthday = datetime.datetime.strptime(birthday, "%d/%m/%Y")
-            person_dict[id_int] = (name, real_birthday)
-        except ValueError as be:
-            print(f"error at birthday {birthday}")
+            person_dict[id] = (name, real_birthday)
+        except ValueError:
+            assignment2.error('Error at ID ' + id + ' with birthday ' + birthday)
 
     return person_dict
 
 
 def displayPerson(id, personData):
-    pass
+    try:
+        info = personData[id]
+        return "The person with ID " + id + " has a birthday of " + info[1].strftime("%d/%m/%Y")
+    except:
+        return "No person with that ID found"
+
 
 def main(url):
     print(f"Running main with URL = {url}...")
-    data_downloaded = downloadData(url)
-    results_dict = processData(data_downloaded)
-    print(results_dict)
+    try:
+        file_content = downloadData(url)
+        personData = processData(file_content)
+        while True:
+            ID = input("Enter ID number to look data or enter <1 to exit: ")
+            if int(ID) > 0:
+                print(displayPerson(ID, personData))
+            else:
+                break
+    except:
+        assignment2.error('Error while getting data from URL, Please try again with different URL')
 
 
 if __name__ == "__main__":
